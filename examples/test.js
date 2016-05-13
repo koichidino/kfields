a2e87e3fab21e141f3a959be80dcd77009c75831
Ubuntu <ubuntu@ip-172-31-9-218.ap-northeast-1.compute.internal>
2016-05-13 03:41:51 +0000

    1. Add options to avoid throw exception.

--- examples/test.js
+++ examples/test.js
@@ -2,4 +2,7 @@ var kfields = require('../index');
 
 var fieldsStr = 'id,profile,test';
 var fields = kfields.parse(fieldsStr);
+
 console.log(fields);
+var test = kfields.parse(fields);
+console.log(test);
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 
-exports.parse = function(fieldsStr) {
-  if (fieldsStr) {
-    var fieldArray = fieldsStr.split(",");
+exports.parse = function(input, options) {
+  if (typeof input === 'string') {
+    var fieldArray = input.split(",");
 
     return fieldArray;
+  } else if (input instanceof Array) {
+    return input;
   } else {
-    throw new Error('Missing fieldsStr in getFields function.');
+    // 要求throw error時才會丟Exception
+    if (options && options.throwError) {
+      throw new Error('Missing input in field parse function.');
+    }
+    return [];
   }
 }
